1593116d2c519ffed9988da227a42ca1d8776e2c
ShureyaNasir <shureyanasir1994@gmail.com>
2022-04-13 14:29:59 +0000

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return (
+      <footer className='page-footer'>
+
+      <div className='footer-copyright'>
+
+      <div className='container'>
+      &copy; Books 2022
+      <a href='#' className='grey-text text-ligten-4 right'>Premest</a>
+      </div>
+      </div>
+      
+    
+      
+   </footer> 
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='navbar-fixed'>
+
+
+    <nav>
+    <div className='appbar'>
+    <a href='#' className='brand-logo'>BOOKS API</a>
+    </div>
+    </nav>
+      
+    </div>
+  )
+}
+
+export default NavBar
